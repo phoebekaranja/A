adf9a6b0afae79edbc7a93fac8bc68a140c55736
phoebekaranja <phoebemurish@gmail.com>
2019-05-24 15:10:15 +0300

    first commit

--- won/views.py
+++ won/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    current_user = request.user
+    projects = Project.get_all()
+    return render(request,'index.html',{'projects':projects})
