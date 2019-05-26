cc079ef54a42757abfc13a87e92036de849b71bc
phoebekaranja <phoebemurish@gmail.com>
2019-05-26 17:42:36 +0300

    created the save delete in models files and modified css file

--- won/models.py
+++ won/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.core.validators import URLValidator
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
-
 class Project(models.Model):
     title = models.CharField(max_length = 50)
     image = models.ImageField(upload_to = 'projects/')
@@ -12,11 +14,35 @@ class Project(models.Model):
     usability=models.PositiveIntegerField(choices=list(zip(range(1, 11), range(1, 11))), default=1)
     content=models.PositiveIntegerField(choices=list(zip(range(1, 11), range(1, 11))), default=1)
     comment =models.TextField(max_length=50,null=True)
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_all(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_project(cls, project_id):
+        project = cls.objects.get(id=project_id)
+        return project
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects_title = cls.objects.filter(title__icontains=search_term)
+        return projects_title
+
 class Profile(models.Model):
     photo = models.ImageField(upload_to = 'profile/')
     profile = models.ForeignKey(User, null=True)
     bio = models.TextField(max_length = 100)
     contact = models.IntegerField(null=True)
+
+
+
     def save_profile(self):
         self.save()
 
@@ -25,4 +51,11 @@ class Profile(models.Model):
 
 
 class Comment(models.Model):
-    comment = models.CharField(max_length = 400)            
+    comment = models.CharField(max_length = 400)
+
+
+    def save_comment(self):
+        self.save()
+    def get_comments_by_images(cls, id):
+        comments = Comment.objects.filter(image_pk = id)
+        return comments
