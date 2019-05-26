b3231c9be39601a9f27c8a0a8d15ecad606d35fb
phoebekaranja <phoebemurish@gmail.com>
2019-05-26 17:53:11 +0300

    added a url pattern

--- won/urls.py
+++ won/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.index,name='indexPage'),
-]
+    url(r'^projects/(\d+)',views.project,name ='projects'),
