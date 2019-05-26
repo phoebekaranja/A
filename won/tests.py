570e7ab3e185f8bb2a1cf000d7d2e5962665a517
phoebekaranja <phoebemurish@gmail.com>
2019-05-26 17:47:09 +0300

    create a setUp method that allows us to create an instance of the Editor class before every test

--- won/tests.py
+++ won/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Profile, Project
+
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.new_profile =Profile(photo="image.jpeg",bio="this is to test if it can work",contact = "0798164370")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
